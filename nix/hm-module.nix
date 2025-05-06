faff268f08210b1ce37b1a7622816007f6ec1027
Elias Ainsworth <pilum-murialis.toge@proton.me>
2025-05-05 22:11:51 -0700

    feat: Add option to enable emacs service through the hm-module.

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -27,13 +27,33 @@ in
 
     };
   };
-
   config = mkIf cfg.enable {
+    # Enable Home Manager packages
     home.packages = [ cfg.package ];
+
+    # Emacs Shell Aliases
+    home.shellAliases = {
+      em = "emacs"; # Open Emacs normally
+      et = "emacs -nw"; # Open Emacs in terminal
+      ec = "emacsclient -c -a ''"; # Open client in a new frame
+      ect = "emacsclient -t -a ''"; # Open client in terminal
+      ek = "emacsclient -e '(kill-emacs)'"; # Kill the Emacs daemon
+      es = "emacs --daemon"; # Start Emacs daemon
+    };
+
     xdg.configFile = mkIf cfg.installAssets {
       "emacs/assets" = {
         source = ../assets;
         recursive = true;
+      };
+    };
+
+    # Conditionally enable Emacs service
+    services.emacs = mkIf cfg.service.enable {
+      enable = true;
+      package = cfg.package;
+      client = {
+        enable = true; # Enable the Emacs client (e.g., emacsclient)
       };
     };
   };
